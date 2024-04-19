ad37c2273fd55f902a2a0e48bd551f2340bfd337
Aravind-B-Kumar <aravindkarullil@gmail.com>
2024-04-19 23:42:49 +0530

    Add option created

---
+++ src/components/Add.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const Add = () => {
+  return (
+    <div>
+        <NavBar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Product Title</label>
+                            <input type="text" className="form-input" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Product Image</label>
+                            <input type="file" name="" id="" className="form-control" /> {/* input:file.form-control */}
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">Description</label>
+                            <textarea name="" id="" cols="30" rows="10" className="form-control"></textarea> {/* textarea.form-control */}
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Category</label>
+                            <select name="" id="" className="form-control">
+                                <option value="men's clothing">men's clothing</option>
+                                <option value="women's clothing">women's clothing</option>
+                                <option value="jewelery">jewelery</option>
+                                <option value="electronics">electronics</option>
+                            </select> {/* select.form-control>option*2  and give the same thing inside value thing*/}
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Price</label>
+                            <input type="number" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-success">Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Add
